7183bcdae1f296a7613f6f40498ce9f3623e0c7b
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-16 23:01:52 +0530

    update

--- Java Basic codes/FibonacciSeries.java
+++ Java Basic codes/FibonacciSeries.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class FibonacciSeries{
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Positive number of terms: ");
