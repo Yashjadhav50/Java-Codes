72fd45409852df1b922077db0a26f8cddcd4b8ab
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-17 19:10:10 +0530

    update

--- Java Basic codes/NumberofDigits.java
+++ Java Basic codes/NumberofDigits.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class NumberofDigits{
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Integers: ");
         int n = sc.nextInt();
