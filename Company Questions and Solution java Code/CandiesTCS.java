b16e2e756f737e4b871af968589cb9281092a7a7
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-18 13:58:42 +0530

    update

--- Company Questions and Solution java Code/CandiesTCS.java
+++ Company Questions and Solution java Code/CandiesTCS.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class CandiesTCS{
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         int n = 10;                                                 // Maximum no of candies in jar
         System.out.println("Total number of Candies in jar: " + n);
