9248decadafa6f019406dfa86c6d53bea00d6182
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-19 10:10:55 +0530

    update

--- Java Basic codes/PositiveNegative.java
+++ Java Basic codes/PositiveNegative.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class PositiveNegative{
     public static void main(String[] args){
+        
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter a number: ");
     int n = sc.nextInt();
