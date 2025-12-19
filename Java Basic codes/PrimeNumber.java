44d7e48a20aa4eb1a61ab290716b7ba58b3b1c74
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-19 10:11:33 +0530

    update

--- Java Basic codes/PrimeNumber.java
+++ Java Basic codes/PrimeNumber.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class PrimeNumber{
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number: ");
         int n = sc.nextInt();
