85fff0fa1690f21430a7ac52620a7478a6a71a12
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-20 12:45:05 +0530

    update

--- Java Basic codes/PrimeNumberRange.java
+++ Java Basic codes/PrimeNumberRange.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class PrimeNumberRange{
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Range of Numbers: ");
         int x = sc.nextInt();
