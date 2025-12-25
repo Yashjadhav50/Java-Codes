d28410ddaedb49ac3b0d4470b8340e7ba7f03ba8
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-25 12:44:43 +0530

    update

--- Java Basic codes/NaturalNumberSum.java
+++ Java Basic codes/NaturalNumberSum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class NaturalNumberSum{
     public static void main(String[] args){
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Natural Number: ");
         int a = sc.nextInt(); 
