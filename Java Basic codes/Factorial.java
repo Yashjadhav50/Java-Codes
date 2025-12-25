ba75b22d0a99fc28f709783736709ec2a66a17b8
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-25 12:44:59 +0530

    update

--- Java Basic codes/Factorial.java
+++ Java Basic codes/Factorial.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Factorial{
     public static void main(String[] args){
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter positive Number: ");
         int f = sc.nextInt();
