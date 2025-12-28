5d61bfce1693bc6a94d4c937f7ac9256d755a878
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-28 23:33:42 +0530

    update

--- Java Basic codes/Factorial.java
+++ Java Basic codes/Factorial.java
@@ -5,7 +5,7 @@ public class Factorial{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter positive Number: ");
         int f = sc.nextInt();
-u
+
         if(f < 0){
             System.out.println("Factorial is not Defined.");
         }else{
