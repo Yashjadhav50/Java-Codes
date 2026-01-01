d0c2cb32c366960d2b25e4fc977bedbeab56a2eb
YASH JADHAV <yashjadhav654321@gmail.com>
2026-01-01 12:24:33 +0530

    u

--- Java Basic codes/CheckCharacterAlphabet.java
+++ Java Basic codes/CheckCharacterAlphabet.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class CheckCharacterAlphabet{
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Character: ");
